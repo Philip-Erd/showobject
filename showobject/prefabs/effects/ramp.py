1f4ae7a1b61b0dbc4fd0f3cab5174cae4e477dd3
philip.erdelhoff <erdelhoffphilip@gmail.com>
2024-12-04 13:39:56 +0100

    fix different start value

--- showobject/prefabs/effects/ramp.py
+++ showobject/prefabs/effects/ramp.py
@@ -8,18 +8,20 @@ class Ramp(Showobject):
         self.value = initial_value
         self._time_passed = 0
         self._duration = 0
-        self._target_value = 0
+        self._end_value = 0
+        self._start_value = 0
 
     def update(self, timer: Timer):
         self._time_passed += timer.dt_ms
         t = self._time_passed / self._duration
         if t <= 1:
-            self.value = t * self._target_value
+            self.value = self._start_value + (t * (self._end_value - self._start_value))
         else:
-            self.value = self._target_value
+            self.value = self._end_value
     
     
     def go(self, value:float, duration_ms: int):
         self._duration = duration_ms
         self._time_passed = 0
-        self._target_value = value
+        self._end_value = value
+        self._start_value = self.value
